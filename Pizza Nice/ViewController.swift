5f2e52ca56b2e9e5daeb42231a9c53dccc991726
Fardi Issihaka <fardi.issihaka@laposte.net>
2021-06-29 01:16:57 +0200

    Initial Commit

---
+++ Pizza Nice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pizza Nice
+//
+//  Created by fardi Clk on 29/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
